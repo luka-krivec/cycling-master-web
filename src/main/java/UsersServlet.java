1c74c6fe41dd152255d4a35f932ffffe590d596d
Luka <luka.zalog@gmail.com>
2015-01-03 01:10:07 +0100

    Add database management for user login.

--- src/main/java/UsersServlet.java
+++ src/main/java/UsersServlet.java
@@ -1,3 +1,5 @@
+import utils.DatabaseUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,8 +11,25 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "UsersServlet", urlPatterns = {"/users/*"})
 public class UsersServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String paramUserSignUp = request.getParameter("usersignup");
+        String paramUserName = request.getParameter("username");
+        String paramEmail = request.getParameter("email");
+        String paramUserPass = request.getParameter("pass");
+        String paramUserAuthType = request.getParameter("authType");
+
+        boolean userSignUp = false;
+
+        if(paramUserSignUp != null && paramUserSignUp.equals("true")) {
+            userSignUp = DatabaseUtils.insertUser(paramUserName, paramEmail, paramUserPass, paramUserAuthType);
+        }
+
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println(userSignUp);
+        out.flush();
+        out.close();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ src/main/java/utils/DatabaseUtils.java
@@ -0,0 +1,58 @@
+package utils;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseUtils {
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://127.2.246.2:3306/cyclingmaster";
+
+    //  Database credentials
+    static final String USER = "admin7ijNKTX";
+    static final String PASS = "Rx1er3VQKjK4";
+
+    public static boolean insertUser(String username, String email, String pass, String authType) {
+        boolean result = false;
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+
+            String sql = "INSERT INTO Users (email, password, username, auth_type) " +
+                    "VALUES (email, PasswordHash.createHash(password), auth_type)";
+            stmt.executeUpdate(sql);
+
+            result = true;
+
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+
+        return result;
+    }
+}
